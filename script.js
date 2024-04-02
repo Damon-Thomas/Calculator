10279d52b21cfc6cc57dbcb0bd1ea938baeaf209
Damon Thomas <damon22t@gmail.com>
2024-04-02 16:08:24 -0400

    add global variables and operate function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b
 }
+
+// returns answer
+function operate() {
+    if (operator === 0) {
+        return add(num1, num2);}
+    else if (operator === 1) {
+        return subtract(num1, num2);}
+    else if (operator === 2) {
+        return multiply(num1, num2);}
+    else if (operator === 3) {
+        return divide(num1, num2);}
+}
+
+
+
+// Global Variables
+let num1 = 0
+let operator = 0 
+let num2 = 0
