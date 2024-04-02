906f7c3e6bab8ac2521f2177cd18b48894c6ddb3
Damon Thomas <damon22t@gmail.com>
2024-04-02 17:01:58 -0400

    add num and operator event listeners to change global variables

--- script.js
+++ script.js
@@ -29,7 +29,50 @@ function operate() {
 
 
 // Global Variables
-let num1 = 0
-let operator = 0 
-let num2 = 0
+let num1 = null
+let operator = null
+let num2 = null
 let displayValue = 0
+
+numbers = document.querySelectorAll(".numbers")
+operators = document.querySelectorAll(".operator")
+
+
+
+// use buttons to set num1 and num2
+numbers.forEach(element => {
+    element.addEventListener('click', function() {
+        if (operator != null) {
+            if (num2 == null) {
+                num2 = ""};
+            num2 += element.textContent;
+        }
+        else {
+            if (num1 == null) {
+                num1 = "";};
+            num1 += element.textContent;
+        }
+    })
+    
+});
+
+// change operator
+operators.forEach(element => {
+    element.addEventListener('click', function() {
+        if (operator == null) {
+            operator = element.textContent
+        }
+    })
+    
+});
+
+
+
+
+
+
+
+
+console.log(num1)
+console.log(operator)
+console.log(num2)
